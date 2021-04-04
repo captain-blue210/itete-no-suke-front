3b5bf71aa964e5221488706264bdd8641ecd99c0
captain-blue210 <captain.blue210@gmail.com>
2021-04-04 18:13:18 +0900

    test : add top page test

--- __test__/top-page.spec.ts
+++ __test__/top-page.spec.ts
@@ -1,3 +1,13 @@
+import Top from '@/pages/index.vue';
+import { RouterLinkStub, shallowMount } from '@vue/test-utils';
+
 describe('top-page', () => {
-  it('should show link', () => {});
+  it('should show link', () => {
+    const wrapper = shallowMount(Top, {
+      stubs: {
+        NuxtLink: RouterLinkStub,
+      },
+    });
+    expect(wrapper.findComponent(RouterLinkStub).props().to).toBe('/user');
+  });
 });
--- pages/user/pain-record/index.vue
+++ pages/user/pain-record/index.vue
@@ -12,6 +12,7 @@
 import { defineComponent } from '@nuxtjs/composition-api';
 
 export default defineComponent({
+  name: 'PainRecord',
   setup() {
     return {};
   },
